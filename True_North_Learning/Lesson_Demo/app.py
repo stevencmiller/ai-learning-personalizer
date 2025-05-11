046ca4c5403ea1577811523e05ed1ac4e2c6271d
stevencmiller <scurtismiller@gmail.com>
2025-05-10 23:10:32 -0600

    Update app.py

--- True_North_Learning/Lesson_Demo/app.py
+++ True_North_Learning/Lesson_Demo/app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
 import streamlit as st
 from utils.progress import log_progress
 from lessons.pythagorean_theorem import run_pythagorean_lesson
